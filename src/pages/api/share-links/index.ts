d396a820818b1dd5754d4524ac659775fcb62ad1
CaedIzaya <Caedmon_Izaya@outlook.com>
2025-12-11 22:00:48 +0800

    Correction 4

--- src/pages/api/share-links/index.ts
+++ src/pages/api/share-links/index.ts
@@ -1,15 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '../../../server/db';
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../../server/auth";
 import { randomBytes } from 'crypto';
+import { getServerAuthSession } from '../../../server/auth';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
-  const session = await getServerSession(req, res, authOptions);
+  const session = await getServerAuthSession({ req, res });
   if (!session) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,6 +3,9 @@ import { type GetServerSidePropsContext } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "~/pages/api/auth/[...nextauth]";
 
+// 统一导出 authOptions，便于在 API 路由中复用同一份配置
+export { authOptions };
+
 export const getServerAuthSession = (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
